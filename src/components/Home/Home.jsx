1ed21052b303a28860d3296cd57f4c7c3a27b240
Ankit Grin <ankit@softradix.com>
2022-08-23 16:41:04 +0530

    fetch shows list done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,17 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../features/movies/movieSlice";
 
 const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
-  const fetchMovies = async () => {
-    const response = await movieApi
-      .get("", { params: { s: movieText, type: "movie" } })
-      .catch((err) => console.log("err ==> ", err));
-    dispatch(addMovies(response.data));
-  };
 
   useEffect(() => {
-    fetchMovies();
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
   }, []);
 
   return (
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,7 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/movieApi";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "Harry";
+    const response = await movieApi.get("", {
+      params: { s: movieText, type: "movie" },
+    });
+    return response.data;
+  }
+);
+
+export const fetchAsyncShows = createAsyncThunk(
+  "movies/fetchAsyncShows",
+  async () => {
+    const seriesText = "Friends";
+    const response = await movieApi.get("", {
+      params: { s: seriesText, type: "series" },
+    });
+    return response.data;
+  }
+);
 
 const initialState = {
   movies: {},
+  shows: {},
 };
 
 const movieSlice = createSlice({
@@ -12,8 +36,25 @@ const movieSlice = createSlice({
       state.movies = payload;
     },
   },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfully!");
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Rejected!");
+    },
+    [fetchAsyncShows.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfully!");
+      return { ...state, shows: payload };
+    },
+  },
 });
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
 export default movieSlice.reducer;
