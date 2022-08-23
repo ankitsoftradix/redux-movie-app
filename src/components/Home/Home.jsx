39d873f67912bc0ecb2db067ab72d77b2bdffdbf
Ankit Grin <ankit@softradix.com>
2022-08-23 12:53:38 +0530

    fetch movies list done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
   const movieText = "Harry";
+  const dispatch = useDispatch();
   const fetchMovies = async () => {
     const response = await movieApi
       .get("", { params: { s: movieText, type: "movie" } })
       .catch((err) => console.log("err ==> ", err));
-    console.log("response ==> ", response);
+    dispatch(addMovies(response.data));
   };
 
   useEffect(() => {
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./MovieCard.scss";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ key, data }) => {
+  return (
+    <div className="card-item" key={key}>
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data.Poster} alt={data.Title} />
+        </div>
+        <div className="card-bottom">
+          <h4>{data.Title}</h4>
+          <p>{data.Year}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
